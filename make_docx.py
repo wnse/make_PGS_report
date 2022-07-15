4e821d2b5e1f5fa7a5a8d938cc4139e16dfc87fd
wnse <yangkai07@gmail.com>
2022-07-15 12:42:19 +0800

    row height

--- make_docx.py
+++ make_docx.py
@@ -38,15 +38,19 @@ def fill_cell(cell, content, font_name='微软雅黑', font_size=10, color=RGBCo
     run.font.size = Pt(font_size)
     run.font.color.rgb = color
 
-def add_row(table, row_no, height=1):
+def add_row(table, row_no, height=None):
     while len(table.rows) < row_no:
         row = table.add_row()
         row.height_rule = WD_ROW_HEIGHT.AT_LEAST
-        row.height = Cm(height)
+        if height:
+            row.height = Cm(height)
 
-def write_table(table, row, col, text, text_vertical=WD_ALIGN_VERTICAL.CENTER, text_para=WD_ALIGN_PARAGRAPH.LEFT, bold=False, font_size=10, line_spacing=16, space_after=8, space_before=8):
+def write_table(table, row, col, text, 
+    text_vertical=WD_ALIGN_VERTICAL.CENTER, text_para=WD_ALIGN_PARAGRAPH.LEFT, 
+    bold=False, font_size=10, line_spacing=16, space_after=8, space_before=8,row_height=None,
+    ):
     if len(table.rows) < row:
-        add_row(table, row)
+        add_row(table, row, height=row_height)
     cell = table.cell(row-1, col-1)
     cell.vertical_alignment = text_vertical
     paragraph = cell.paragraphs[0]
@@ -103,15 +107,16 @@ def make_PGS_report(dict_family, dict_sample, dict_config, outdir='./', png_dir=
                             dict_config[idx]['col'], 
                             f'{idx_info}', 
                             bold=bold, 
-                            text_para=WD_ALIGN_PARAGRAPH.CENTER
+                            text_para=WD_ALIGN_PARAGRAPH.CENTER,
+                            row_height=1
                             )
                 if png_dir:
-                    write_table(tables[4], row_no*4+1, 1, f"样本编号：", bold=True, font_size=11)
-                    write_table(tables[4], row_no*4+2, 1, f"检测结果：", bold=True, font_size=11)
-                    write_table(tables[4], row_no*4+3, 1, f"染色体拷贝数结果", bold=True, font_size=11)
+                    write_table(tables[4], row_no*3+1, 1, f"样本编号：", bold=True, font_size=11)
+                    write_table(tables[4], row_no*3+2, 1, f"检测结果：", bold=True, font_size=11)
+                    write_table(tables[4], row_no*3+3, 1, f"染色体拷贝数结果", bold=True, font_size=11)
 
-                    write_table(tables[4], row_no*4+1, 1, f"{s_info['样本编号']}")
-                    write_table(tables[4], row_no*4+2, 1, f"{s_info['检测结果']}")
+                    write_table(tables[4], row_no*3+1, 1, f"{s_info['样本编号']}")
+                    write_table(tables[4], row_no*3+2, 1, f"{s_info['检测结果']}")
                     if png_name == 2:
                         pngName = '_new_2color'
                     elif png_name == 3:
@@ -120,7 +125,7 @@ def make_PGS_report(dict_family, dict_sample, dict_config, outdir='./', png_dir=
                         pngName = ''
                     pic_path = os.path.join(pngdir, f'PGTA_{s}.fq_merge_all_chrom{pngName}.png')
                     try:
-                        insert_pic(tables[4], row_no*4+4, 1, pic_path)
+                        insert_pic(tables[4], row_no*3+3, 1, pic_path)
                     except Exception as e:
                         logging.error(e)
                 row_no += 1
@@ -135,7 +140,7 @@ if __name__ == '__main__':
     parse.add_argument('-c', '--config', default=os.path.join(bin_dir, 'template_config.xlsx'), help='config excel for table information')
     parse.add_argument('-t', '--template', default=os.path.join(bin_dir, 'template.docx'), help='report template docx file')
     parse.add_argument('-o', '--outdir', default='./', help='report output directory')
-    parse.add_argument('-p', '--png_name', default=1, choices=[1, 2], type=int, help='1 for default, 2 for 2color')
+    parse.add_argument('-p', '--png_name', default=1, choices=[1, 2, 3], type=int, help='1 for default, 2 for 2color')
     parse.add_argument('-f', '--figure_dir', default=None, help='figure directory')
     args = parse.parse_args()
     
